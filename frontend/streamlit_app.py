cf217de907ad18741f89f0b59c99cced18dc0c1d
Federico Castañeda <fedecasta@gmail.com>
2024-03-18 10:44:34 -0300

    First tests streamlit

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -1,1 +1,9 @@
 
+import streamlit as st
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+if st.checkbox('Enter your name'):
+    st.text_input("Your name", key="name")
+    # You can access the value at any point with:
+    st.write( "Name:", st.session_state.name)
