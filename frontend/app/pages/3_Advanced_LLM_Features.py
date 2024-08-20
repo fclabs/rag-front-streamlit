8ff2edf293553ada080a786582dfbc7db5e6a3cb
Federico Castañeda <fedecasta@gmail.com>
2024-08-20 08:07:30 -0300

    update

--- frontend/app/pages/3_Advanced_LLM_Features.py
+++ frontend/app/pages/3_Advanced_LLM_Features.py
@@ -5,14 +5,33 @@ from langchain_openai import ChatOpenAI
 
 import streamlit as st
 
+FEW_SHOT_PROMPT = """
+You are a film critic and you are writing a review index based on user test description. Answer only with
+the review index. Revire index is a number between 1 and 5, where 1 is the worst and 5 is the best.
+
+Examples:
+User: "The movie was great, I loved the action scenes!"
+Answer: 5
+
+User: "The movie was boring and the acting was terrible."
+Answer: 1
+
+User: "The movie was okay, but the ending was disappointing."
+Answer: 3
+
+User: {question}
+"""
+
+
+
 def get_response(chain, prompt , config):
     return chain.stream({"question": prompt}, config)
 
-st.write("## Tuneable Chat with History")
+st.write("## Tuneable Few Shots Movie Critique Chatbot")
 
 col1, col2 = st.columns([1, 2])
 with col1:
-    system_prompt = st.text_area("System Prompt", "You are an AI chatbot having a conversation with a human.")
+    system_prompt = st.text_area("System Prompt", FEW_SHOT_PROMPT, height=200)
     st.button("Clear History", on_click=lambda: msgs.clear())
 with col2:
     st.text("ID of the model to use")
@@ -23,34 +42,22 @@ with col2:
 
 # Set up memory
 msgs = StreamlitChatMessageHistory(key="langchain_messages")
-if len(msgs.messages) == 0:
-    msgs.add_ai_message("How can I help you?")
 
 # Set up the LangChain, passing in Message History
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
-        MessagesPlaceholder(variable_name="history"),
         ("human", "{question}"),
     ]
 )
 model_params = dict(model=model, temperature=temperature, max_tokens=max_token)
 llm_model = ChatOpenAI(**model_params)
 chain = prompt | llm_model
-chain_with_history = RunnableWithMessageHistory(
-    chain,
-    lambda _: msgs,
-    input_messages_key="question",
-    history_messages_key="history",
-)
 
-# Render current messages from StreamlitChatMessageHistory
-for msg in msgs.messages:
-    st.chat_message(msg.type).write(msg.content)
 
 # If user inputs a new prompt, generate and draw a new response
 if prompt := st.chat_input():
     st.chat_message("human").write(prompt)
     # Note: new messages are saved to history automatically by Langchain during run
     config = {"configurable": {"session_id": "any"}}
-    st.chat_message("ai").write_stream(chain_with_history.stream({"question": prompt}, config))
+    st.chat_message("ai").write_stream(chain.stream({"question": prompt}, config))
